cd8c2d0207aec14fcf915710593e5e0c502d7ed1
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-02 13:16:54 -0500

    feat(darken): add darken command

--- index.js
+++ index.js
@@ -6,10 +6,32 @@ const Color = require('color')
 program
     .command('whiten <color>')
     .option('-f, --factor <[0:1]>', 'whitening factor')
-    .action(function (color, cmd) {
-        const c = Color(color)
-        const lighter = c.mix(Color('white'), cmd.factor)
-        console.log(lighter.hex())
-    })
+    .action(mixColorWith('white'))
+
+program
+    .command('darken <color>')
+    .option('-f, --factor <[0:1]>', 'darkening factor')
+    .action(mixColorWith('black'))
+
 program.parse(process.argv)
+
+function mixColorWith(toMixWith) {
+    return function (color, cmd) {
+        mixAndPrintColor(color, toMixWith, cmd.factor)
+    }
+}
+
+function mixAndPrintColor(color, toMixWith, factor) {
+    const mixed = mixColor(color, toMixWith, factor)
+    printColor(mixed)
+}
+
+function mixColor(color, toMixWith, factor) {
+    const c = Color(color)
+    return c.mix(Color(toMixWith), factor)
+}
+
+function printColor(color) {
+    console.log(color.hex())
+}
 
